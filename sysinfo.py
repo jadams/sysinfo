a10b3654824e2300791191f8f57807cf5c97e1cd
John Adams <john@hexb.it>
2016-02-08 15:17:54 -0500

    datetime func

--- sysinfo.py
+++ sysinfo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os, sys, subprocess
+import os, sys, subprocess, datetime
 
 def get_kernel():
     kernel =  subprocess.check_output(['uname', '-r']).decode('utf-8').strip('\n')
@@ -19,7 +19,14 @@ def get_uptime():
     uptime = uptime.split()
     return {'days':int(uptime[1]), 'hours':int(uptime[3]), 'mins':int(uptime[5])}
 
+def get_date():
+    dt = str(datetime.datetime.now()).split()
+    dt[1] = dt[1].split('.')[0]
+    return dt
+
+
 if __name__ == '__main__':
     print(get_kernel())
     print(get_fqdn())
     print(get_uptime())
+    print(get_date())
