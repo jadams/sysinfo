a11f2ca63a51710d56fa1778a763eef9c04d0e29
John Adams <john@hexb.it>
2016-02-08 14:57:36 -0500

    kernel func

--- sysinfo.py
+++ sysinfo.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
+import os, sys, subprocess
+
+def get_kernel():
+    kernel =  subprocess.check_output(['uname', '-r']).decode('utf-8').strip('\n')
+    kernel = kernel.split('-')
+    return kernel
+
 if __name__ == '__main__':
+    print(get_kernel())
     print
