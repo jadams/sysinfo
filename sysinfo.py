0f9c0c0d7ea51db5b5a041f8cc257acafc8f2b2e
John Adams <john@hexb.it>
2016-02-08 15:46:23 -0500

    ip addresses func

--- sysinfo.py
+++ sysinfo.py
@@ -24,9 +24,25 @@ def get_date():
     dt[1] = dt[1].split('.')[0]
     return dt
 
+def get_ip_addr():
+    addr = subprocess.check_output(['ip', 'addr']).decode('utf-8').strip('\n')
+    addr = addr.split('\n')
+    ip = {}
+    for line in addr:
+        if line[0].isdigit():
+            iface = line.split()[1].strip(':')
+            ip[iface] = {'ipv6':[], 'ipv4':[]}
+        if 'inet6' in line:
+            address = line.split()[1].split('/')
+            ip[iface]['ipv6'].append(address)
+        elif 'inet' in line:
+            address = line.split()[1].split('/')
+            ip[iface]['ipv4'].append(address)
+    return ip
 
 if __name__ == '__main__':
     print(get_kernel())
     print(get_fqdn())
     print(get_uptime())
     print(get_date())
+    print(get_ip_addr())
